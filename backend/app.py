0c22c0e37435becf8c1dbed00c4f5d632e238d08
PranshuDixitDev <137303794+PranshuDixitDev@users.noreply.github.com>
2024-05-28 16:52:41 +0530

    print statements removed

--- backend/app.py
+++ backend/app.py
@@ -12,8 +12,6 @@ load_dotenv()
 database_url = os.environ.get('DATABASE_URL')
 if not database_url:
     raise ValueError("DATABASE_URL environment variable not set")
-else:
-    print(f"Database URL: {database_url}")
 
 app = Flask(__name__)
 CORS(app)  # Enables CORS for all domains; adjust as necessary
@@ -22,10 +20,8 @@ CORS(app)  # Enables CORS for all domains; adjust as necessary
 def get_db_connection():
     try:
         conn = psycopg2.connect(database_url, cursor_factory=RealDictCursor)
-        print("Database connection successful")
         return conn
     except Exception as e:
-        print(f"Database connection error: {e}")
         raise
 
 @app.route('/api/contact', methods=['POST'])
@@ -43,10 +39,8 @@ def handle_contact():
                     (name, email, phone_number, message))
         new_message = cur.fetchone()
         conn.commit()
-        print("New message inserted:", new_message)
         return jsonify(new_message), 201
     except Exception as e:
-        print(f"Error inserting message: {e}")
         return jsonify({"error": "Internal Server Error"}), 500
     finally:
         cur.close()
